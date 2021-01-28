91f9ac1681449a4c729c5f1394b45cdf2aed7c81
debora7376 <devsouza.root@gmail.com>
2021-01-28 16:54:40 -0300

    Curso JPA parte 2

---
+++ src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
@@ -0,0 +1,36 @@
+package br.com.alura.jpa.modelo.dao;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.MediaComData;
+
+public class MovimentacaoDao {
+
+	private EntityManager em;
+	
+	public MovimentacaoDao(EntityManager em) {
+		this.em = em;
+	}
+
+	public List<MediaComData> getMediaDiariaDasMovimentacoes() {
+		String jpql = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m "
+						+ "group by day(m.data), month(m.data), year(m.data)";
+
+		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
+		return query.getResultList();
+	}
+	
+	public BigDecimal getSomaDasMovimentacoes() {
+		String jpql = "select sum(m.valor) from Movimentacao m ";
+
+		TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
+		BigDecimal somaDasMovimentacoes = query.getSingleResult();
+		
+		return somaDasMovimentacoes;
+	}
+
+}
--- src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
@@ -5,20 +5,17 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaMediaDiariaDasMovimentacoes {
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
 		
-		String jpql = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m "
-						+ "group by day(m.data), month(m.data), year(m.data)";
-
-		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
-		List<MediaComData> mediaDasMovimentacoes = query.getResultList();
+		List<MediaComData> mediaDasMovimentacoes = new MovimentacaoDao(em).getMediaDiariaDasMovimentacoes();
 		
 		for (MediaComData resultado : mediaDasMovimentacoes) {
 			System.out.println("A média das movimentacoes do dia " + resultado.getDia() + "/" + resultado.getMes() + " é: " + resultado.getValor());
